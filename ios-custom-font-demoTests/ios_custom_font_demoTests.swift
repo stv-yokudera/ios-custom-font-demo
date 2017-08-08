483ead40c2f85a267925ec11311dca2077018b84
y.okudera <mail@sample.com>
2017-08-09 08:30:40 +0900

    Initial Commit

---
+++ ios-custom-font-demoTests/ios_custom_font_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_custom_font_demoTests.swift
+//  ios-custom-font-demoTests
+//
+//  Created by OkuderaYuki on 2017/08/09.
+//  Copyright © 2017年 Okudera Yuki. All rights reserved.
+//
+
+import XCTest
+@testable import ios_custom_font_demo
+
+class ios_custom_font_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
